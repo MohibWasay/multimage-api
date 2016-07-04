dd7cdb7ed4dc4faef0e2d87b2d820416e746e15c
Mohib A. Wasay <wasay_siddiqui81@yahoo.com>
2016-07-04 14:55:40 +0500

    initialized config file

---
+++ config/config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var cloudinary = require('cloudinary');
+var process = require('process');
+var _config;
+
+var envs = {
+	dev: {
+		env: 'dev',
+		db: 'feed_db',
+		hostname: 'localhost',
+		cloudinary: {
+			cloudName: 'feed',
+			apiKey: '395845624313527',
+			apiSecret: 'rP5FRFCwOiSgEherG_JmsTjuNDw'
+		},
+		secret: 'blaa123'
+	},
+	prod: {
+		env: 'dev',
+		db: 'feed_db',
+		hostname: 'localhost'
+	}
+};
+
+function init(){
+	return new Promise((resolve)=>{
+		var _env = env();
+		console.log('Initializing '+_env+' environment');
+		_config = envs[_env];
+		cloudinary.config({
+			cloud_name: _config.cloudinary.cloudName,
+			api_key: _config.cloudinary.apiKey,
+			api_secret: _config.cloudinary.apiSecret
+		});
+		resolve();
+	});
+}
+
+function config(){
+	return envs[env()];
+}
+
+function env(){
+	return process.argv[2] || 'dev'
+}
+
+module.exports.init = init;
+module.exports.config = config;
