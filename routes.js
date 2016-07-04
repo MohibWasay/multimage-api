83fae3ab09246fb2a7774c7a285a672f4fd726c0
Mohib Wasay <wasay_siddiqui81@yahoo.com>
2016-07-05 02:23:05 +0500

    made changes to the router

--- routes.js
+++ routes.js
@@ -1,16 +1,32 @@
 'use strict';
 
 var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var upload = multer({ storage : storage }).array('files',10);
+
+var storage =   multer.diskStorage({
+  destination: destination,
+  filename: filename
+});
+
+function destination(req, file, callback) {
+    callback(null, './uploads');
+}
+
+function filename(req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+}
 
 function init(app){
 	app.get('/', hello)
-	app.post('/upload', upload.array(), uploadImages);
+	app.post('/upload', uploadImages);
 	console.log("Routes initiated");
 }
 
 function uploadImages(req, res, next){
-	console.log(req, res, next);
+	upload(req,res,function(err) {
+        if(err)	return res.end("Error uploading file.");
+        res.end("File is uploaded");
+    });
 }
 
 function hello(req, res, next){
