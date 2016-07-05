b434b08d2d18d9b52316863dee255d4f4e006c1d
Mohib Wasay <wasay_siddiqui81@yahoo.com>
2016-07-05 09:21:59 +0500

    uploader controller methods

--- controller/uploader.controller.js
+++ controller/uploader.controller.js
@@ -2,17 +2,21 @@ var db = require('mongoose');
 var Uploader = require('../schema/uploader');
 var _ = require('lodash');
 
-function upload(req, res) {
-	if(!req.file && req.file.filename)
+function upload(req, res, next) {
+	if(!req.files)
 		return res.status(400).send({status: 400, message: 'Cannot Upload Image(s)', status: false});
 	let uploader = new Uploader();
-	let postData = req.body;
-	
-	
-	feed.create(req.file.filename).then((feed)=>{
-		res.status(200).send({status: 200, message: 'Successfully Uploaded Image(s)', status: true});
+	_.each(req.files, (file)=>{
+		let postData = file;
+		postData.id = file.filename;
+		postData.name = file.originalname;
+	})
+
+	uploader.upload(req.files).then((files)=>{
+		res.send({status: 200, message: 'Successfully Uploaded Image(s)', files: files});
 	},(err)=>{
-		res.status(200).send({status: 500, message: 'Internal Server Error', status: false});
-	});
+		res.send({status: 500, message: 'Internal Server Error', err: err});
+	})
 }
 
+exports.upload = upload;
