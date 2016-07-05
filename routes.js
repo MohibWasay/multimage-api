ed3ab945de620800d48104fa5b1801177a60f9de
Mohib Wasay <wasay_siddiqui81@yahoo.com>
2016-07-05 09:22:29 +0500

    fixed routing, cleaned up using multer

--- routes.js
+++ routes.js
@@ -1,36 +1,17 @@
 'use strict';
 
 var multer  = require('multer');
-var upload = multer({ storage : storage }).array('files',10);
-
-var storage =   multer.diskStorage({
-  destination: destination,
-  filename: filename
-});
-
-function destination(req, file, callback) {
-    callback(null, './uploads');
-}
-
-function filename(req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
-}
+var uploader = require('./controller/uploader');
+var upload = multer({ dest: 'uploads/' })
 
 function init(app){
 	app.get('/', hello)
-	app.post('/upload', uploadImages);
+	app.post('/upload', upload.any(), uploader.upload);
 	console.log("Routes initiated");
 }
 
-function uploadImages(req, res, next){
-	upload(req,res,function(err) {
-        if(err)	return res.end("Error uploading file.");
-        res.end("File is uploaded");
-    });
-}
-
 function hello(req, res, next){
-	res.send('Hello World');
+	res.send('Hello World')
 }
 
 module.exports.init = init;
