7f6bb200ba81ba2a522bcf073abadb78668cd21d
Mohib A. Wasay <wasay_siddiqui81@yahoo.com>
2016-07-05 13:22:26 +0500

    updated controller uploader

--- controller/uploader.js
+++ controller/uploader.js
@@ -5,12 +5,28 @@ var _ = require('lodash');
 function upload(req, res, next) {
 	if(!req.files)
 		return res.status(400).send({status: 400, message: 'Cannot Upload Image(s)', status: false});
-	var uploader = new Uploader();
-	uploader.upload(req.files).then((files)=>{
+	var files = [];
+	_.each(req.files, (file)=>{
+		files.push(new Uploader({id: file.filename, name: file.originalname}));
+	})
+
+	Uploader.upload(files).then((files)=>{
 		res.send({status: 200, message: 'Successfully Uploaded Image(s)', files: files});
 	},(err)=>{
 		res.send({status: 500, message: 'Internal Server Error', err: err});
 	})
 }
 
+function rename(req, res, next){
+	var oldName = req.body.oldName;
+	var newName = req.body.newName;
+	Uploader.findOneAndUpdate({name: oldName},{$set: {name: newName}},(err, upload)=>{
+		if(err){
+			res.send({status: 500, message: 'Update Name Failed'});
+		}
+		res.send({status: 200, message: 'Successfully Updated Name', image: upload});
+	})
+}
+
 exports.upload = upload;
+exports.rename = rename;
